db26fbb82a4bb1cbe00c10f61163db3d654916b3
Brad Treloar <brad@treloardigital.com.au>
2021-01-25 00:14:16 +1030

    misc styling

--- web/app/themes/sage/resources/views/layouts/app.blade.php
+++ web/app/themes/sage/resources/views/layouts/app.blade.php
@@ -1,19 +1,24 @@
 <!doctype html>
 <html {!! get_language_attributes() !!}>
   @include('partials.head')
-  <body @php body_class() @endphp>
+  <body @php body_class('bg-light') @endphp>
     @php do_action('get_header') @endphp
-    @include('partials.header')
-    <div class="wrap container" role="document">
-      <div class="content">
-        <main class="main">
-          @yield('content')
-        </main>
-        @if (App\display_sidebar())
-          <aside class="sidebar">
-            @include('partials.sidebar')
-          </aside>
-        @endif
+    @include('partials.svg')
+    <div class="d-lg-flex flex-row-reverse">
+      <div class="navbar-wrapper">
+        @include('partials.header')
+      </div>
+      <div class="flex-grow-1" role="document">
+        <div class="container-md">
+          <main class="main">
+            @yield('content')
+          </main>
+          @if (App\display_sidebar())
+            <aside class="sidebar">
+              @include('partials.sidebar')
+            </aside>
+          @endif
+      </div>
       </div>
     </div>
     @php do_action('get_footer') @endphp
