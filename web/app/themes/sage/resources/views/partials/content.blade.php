1015450cef7c6f3ac77cdab8f518dee1a0eaa410
Brad Treloar <brad@treloardigital.com.au>
2021-01-25 23:50:15 +1030

    add archive_block shortcode

--- web/app/themes/sage/resources/views/partials/content.blade.php
+++ web/app/themes/sage/resources/views/partials/content.blade.php
@@ -1,9 +1,35 @@
 <article @php post_class() @endphp>
-  <header>
-    <h2 class="entry-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h2>
-    @include('partials/entry-meta')
-  </header>
-  <div class="entry-summary">
-    @php the_excerpt() @endphp
+  <div class="card border-0 p-2 p-md-3">
+    <div class="row">
+      <div class="col-md-4">
+        <div class="mb-3 mb-md-0">
+          <a class="d-block text-decoration-none" href="{{ get_permalink() }}">
+            {{ the_post_thumbnail('medium', ['class' => 'w-100 h-auto']) }}
+          </a>
+        </div>
+      </div>
+      <div class="col-md-8">
+        <div>
+          <h2>
+            {!! get_the_title() !!}
+          </h2>
+        </div>
+        <div class="tags my-2">
+          @foreach (get_field("tags") as $term)
+            <span class="badge badge-info">
+              {{ $term->name }}
+            </span>
+          @endforeach
+        </div>
+        <div class="entry-summary my-2">
+          @php the_field("summary") @endphp
+        </div>
+        <div>
+          <a href="{{ get_permalink() }}">
+            <small>Read more</small>
+          </a>
+        </div>
+      </div>
+    </div>
   </div>
 </article>
